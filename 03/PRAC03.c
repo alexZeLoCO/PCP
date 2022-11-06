0619abd7b890824b6532a63b77ad0b3664448f30
UO281827 <UO281827@uniovi.es>
2022-11-06 10:29:02 +0100

    Added PRAC03

--- 03/PRAC03.c
+++ 03/PRAC03.c
@@ -70,28 +70,6 @@ double* pmT (int m, int n, int k, double alpha, double *A, int lda, double *B, i
 	return C;
 }
 
-// --- PRODUCTO MATRICIAL --- PARALLEL FOR ---
-double* pmFor (int m, int n, int k, double alpha, double *A, int lda, double *B, int ldb, double beta, double *C, int ldc)
-{
-	double tmp;
-	int i, j, p;
-	#pragma omp parallel for private(j, tmp, p)
-	for (i = 0 ; i < m ; i++) {	// fil
-		for (j = 0 ; j < n ; j++) { // col
-			// C[i][j] = beta * C[i][j];
-			*(C+j*ldc+i) = beta * *(C+j*ldc+i);
-			tmp = 0.0;
-			for (p = 0 ; p < k ; p++) { // col A y fil B
-				// tmp += A[i][p] * B[p][i]
-				tmp+=*(A+p*lda+i) * *(B+j*ldb+p);
-			}
-			// C[i][j] += alpha * tmp;
-			*(C+j*ldc+i) += alpha * tmp;
-		}	
-	}
-	return C;
-}
-
 // --- MATRIZ T --- PARALLEL FOR ---
 double* tspFor (int m, int k, double* A)
 {
@@ -107,7 +85,7 @@ double* tspFor (int m, int k, double* A)
 }
 
 // --- PRODUCTO MATRICIAL T --- PARALLEL FOR ---
-double* pmForT (int m, int n, int k, double alpha, double *A, int lda, double *B, int ldb, double beta, double *C, int ldc)
+double* MyDGEMM (int tipo, int m, int n, int k, double alpha, double *A, int lda, double *B, int ldb, double beta, double *C, int ldc)
 {
 	double tmp;
 	int i, j, p;
@@ -129,31 +107,6 @@ double* pmForT (int m, int n, int k, double alpha, double *A, int lda, double *B
 	return C;
 }
 
-// --- PRODUCTO MATRICIAL --- PARALLEL TASK ---
-double* pmTask (int m, int n, int k, double alpha, double *A, int lda, double *B, int ldb, double beta, double *C, int ldc)
-{
-	double tmp;
-	int i, j, p;
-	#pragma omp parallel private (i, j, tmp, p)
-	#pragma omp single
-	for (i = 0 ; i < m ; i++) {	// fil
-		#pragma omp task
-		for (j = 0 ; j < n ; j++) { // col
-			// C[i][j] = beta * C[i][j];
-			*(C+j*ldc+i) = beta * *(C+j*ldc+i);
-			tmp = 0.0;
-			for (p = 0 ; p < k ; p++) { // col A y fil B
-				// tmp += A[i][p] * B[p][i]
-				tmp+=*(A+p*lda+i) * *(B+j*ldb+p);
-			}
-			// C[i][j] += alpha * tmp;
-			*(C+j*ldc+i) += alpha * tmp;
-		}	
-	}
-	return C;
-}
-
-// --- T MATRIZ --- PARALLEL TASK ---
 double* tspTask (int m, int k, double* A)
 {
 	int i, j;
@@ -170,11 +123,11 @@ double* tspTask (int m, int k, double* A)
 }
 
 // --- PRODUCTO MATRICIAL T --- PARALLEL TASK ---
-double* pmTaskT (int m, int n, int k, double alpha, double *A, int lda, double *B, int ldb, double beta, double *C, int ldc)
+double* MyDGEMMT (int tipo, int m, int n, int k, double alpha, double *A, int lda, double *B, int ldb, double beta, double *C, int ldc)
 {
 	double tmp;
 	int i, j, p;
-	A = tspFor (m, k, A);
+	A = tspTask (m, k, A);
 	#pragma omp parallel private (i, j, tmp, p) // ==> El siguiente bloque (single) se ejectua con varios hilos
 	#pragma omp single // ==> Solo un hilo ejecuta el siguiente bloque (for)
 	for (i = 0 ; i < m ; i++) {	// fil
@@ -194,53 +147,26 @@ double* pmTaskT (int m, int n, int k, double alpha, double *A, int lda, double *
 	return C;
 }
 
-// --- PRODUCTO MATRICIAL --- PARALLEL BLOCK ---
-// Matrices cuadradas ==> m = n = k
-double* pmBlock (int m, int n, int k, double alpha, double *A, int lda, double *B, int ldb, double beta, double *C, int ldc, int blk)
+void blockDGEMM (int n, int blk, int ld, double alpha, double *A, double *B, double *C, int blk_column, int blk_row)
 {
+	int i, j, k, total_size = n*n, blk_column_start = blk_column * blk * ld, blk_row_start blk_row * blk;
 	double tmp;
-	int i, j, p;
-	int size;
-	size = n/blk;
-	
-	// Escalar
-	for (i = 0 ; i < n*n ; i++)
-		*(C+i) = beta * *(C+i);
-/*
- * A B C D E
- * A B C D E
- * A B C D E
- * A B C D E
- */
-	for (j = 0 ; j < size ; j++) // Col
+	for (i = 0 ; i < blk ; i++) // i = idx col 
 	{
-		for (i = 0 ; i < m ; i+=blk) // Block
-		{
-			for (p = 0 ; p < m; p+=blk)
-				pm(blk, blk, blk, alpha, A+(p*lda+i), lda, B+(i*ldb+p), ldb, beta, C+(j*ldc+i), ldc);
-		}
-	}
-	return C;
-}
-
-void blockDGEMM (int n, int m, int k, double alpha, double* A, int lda, double* B, int ldb, double* C, int ldc, int blk)
-{
-	int i, j, p;
-	double tmp;
-	for (i = 0 ; i < blk ; i+=blk)
-	{
-		for (j = 0 ; j < blk ; j+=blk)
+		for (j = 0 ; j < blk ; j++) // j = idx fil 
 		{
 			tmp = 0.0;
-			for (p = 0 ; p < lda ; p++)
-				tmp+= *(A+i*lda+p) * *(B+p*ldb+i);
-		}
+			for (k = 0 ; k < total_size ; k++)
+				tmp+= *(A+k+(i+blk_column_start)*ld) * *(B+j+blk_row_start+k*ld);
+
+			*(C+j+i*ld) += alpha * tmp;
+		}	
 	}
 }
 
 double MyDGEMMB (int tipo, int m, int n, int k, double alpha, double *A, int lda, double *B, int ldb, double beta, double *C, int ldc, int blk)
 {
-	int i, j, p, n_blocks_row = n/blk;
+	int i, j, n_blocks_row = n/blk;
 	
 	// Escalar
 	for (i = 0 ; i < m*m ; i++)
@@ -252,31 +178,23 @@ double MyDGEMMB (int tipo, int m, int n, int k, double alpha, double *A, int lda
 	for (i = 0 ; i < n ; i++)
 	{
 		for (j = i+1 ; j<n ; j++)
-			// A[i*lda+j] = A[j*lda+i]
+			// A[i*lda+j] = A[j*lda+i];
 			*(A+i*lda+j) = *(A+j*lda+i);
 	}
 */
 	A = tsp (m, k, A);
-/*
 	for (i = 0 ; i < n_blocks_row ; i++)
 	{
 		for (j = 0 ; j < n_blocks_row ; j++)
 		{
-			blockDGEMM(n, m, k, alpha, A, lda, B, ldb, C+i*blk+blk*lda+j, ldc, blk);
+			blockDGEMM (n, blk, ldc, alpha, A, B, C, i, j);
 		}
 	}
-*/
-	for (i = 0 ; i < m ; i+=blk)
-	{
-		for (j = 0 ; j < n ; j+=blk)
-		{
-			for (d = 0 ; d < k ; d+=blk)
-				blockDGEMM(n, m, k, alpha, A+p*lda+i, lda, B+j*ldb+d, ldb, C+j*ldc+i, ldc, blk);
-		}
-	}
+	
 	return 0;
 }
 
+/*
 double MyDGEMM(int tipo, int m, int n, int k, double alpha, double *A, int lda, double *B, int ldb, double beta, double *C, int ldc)
 {
   double timeini, timefin;
@@ -295,7 +213,7 @@ double MyDGEMM(int tipo, int m, int n, int k, double alpha, double *A, int lda, 
     case TransA:
       timeini=Ctimer();  
       // llamada a la funcion del alumno que trabaja con la transpuesta. Se simula con un timer (sleep)
-      // pmTransp(m, n, k, alpha, A, lda, B, ldb, beta, C, ldc);
+      pmTransp(m, n, k, alpha, A, lda, B, ldb, beta, C, ldc);
       sleep(0.5);
       timefin=Ctimer()-timeini;
       break;
@@ -304,3 +222,4 @@ double MyDGEMM(int tipo, int m, int n, int k, double alpha, double *A, int lda, 
   }
   return timefin;
 }
+*/
